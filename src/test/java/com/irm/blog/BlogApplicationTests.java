0a9a18d1231dbdd45427e5ff53d88499829fa9b6
lmwAndy <35892497+lmwAndy@users.noreply.github.com>
2021-08-10 14:24:05 +0800

    first commit

---
+++ src/test/java/com/irm/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.irm.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
