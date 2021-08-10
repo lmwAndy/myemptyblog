42220cb8c1252e36b111b34e5f104bc828485b84
lmwAndy <35892497+lmwAndy@users.noreply.github.com>
2021-08-10 14:35:16 +0800

    first commit

--- src/main/java/com/irm/Test/TestDemo.java
+++ src/main/java/com/irm/Test/TestDemo.java
@@ -6,6 +6,7 @@ public class TestDemo {
         System.out.println("hello");
         System.out.println("hello2");
         System.out.println("hello3");
+        System.out.println("hello4");
 
     }
 
